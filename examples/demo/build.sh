2abd09b1d9e0d8a158cda398897249e468cfdd2f
Megh Rathod <me@meghrathod.dev>
2025-11-19 04:03:51 +0000

    Update build script for A30 GPU support and maybe robust???

--- examples/demo/build.sh
+++ examples/demo/build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 mkdir -p build
-nvcc  --gpu-architecture=compute_70 --gpu-code=sm_75,sm_70 --compiler-options '-fPIC' -o ./build/libdemo.so --shared ./demo.cu
 
-g++ -g -O0 main.cpp -L./build -ldemo -o ./build/main -Wl,-rpath,${NEGATIVA_ML_PATH}/examples/demo/build
+# A30 is compute capability 8.0
+nvcc \
+  --gpu-architecture=compute_80 \
+  --gpu-code=sm_80 \
+  --compiler-options '-fPIC' \
+  -o ./build/libdemo.so \
+  --shared ./demo.cu
+
+# Link the main executable
+g++ -g -O0 main.cpp -L./build -ldemo -o ./build/main \
+  -Wl,-rpath,'$ORIGIN'
